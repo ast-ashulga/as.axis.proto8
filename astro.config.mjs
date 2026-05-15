5e9ec5685dca686800d523cf394c695dfad46c08
ashulga <ashulga.app@gmail.com>
2026-05-15 22:01:47 +0300

    Deploy Astro app to GitHub Pages via Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://ast-ashulga.github.io',
+  base: '/as.axis.proto8',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
